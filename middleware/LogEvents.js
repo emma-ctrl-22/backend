e04861e4d7ffa41e450f793440996598e8339896
ellie-ctrl35 <146957037+ellie-ctrl35@users.noreply.github.com>
2024-02-11 02:11:13 +0000

    Add logEvents middleware for logging events

--- middleware/LogEvents.js
+++ middleware/LogEvents.js
@@ -4,3 +4,16 @@ const {v4: uuid} = require('uuid');
 const fs = require('fs');
 const fsPromises = fs.promises;
 const path = require('path');
+
+const logEvents = async (message) => {
+  const dateTime = `${format(new Date(), 'yyyy-MM-dd HH:mm:ss')}`;
+  const logMessage = `${dateTime}\t${uuid}\t${message}\n`;
+  try {
+    if (!fs.existsSync(path.join(__dirname, '../logs'))) {
+      fs.mkdirSync(path.join(__dirname, '../logs'));
+    }
+    await fsPromises.appendFile(path.join(__dirname, '../logs/events.log'), logMessage);
+  } catch (error) {
+   console.log('Error writing to log file', error); 
+  }
+}
