d2f07a00523f5679d43432fd7cc9dd5d32b2f2dc
ellie-ctrl35 <146957037+ellie-ctrl35@users.noreply.github.com>
2024-02-12 03:05:31 +0000

    Add authRoute to handle authentication requests

--- index.js
+++ index.js
@@ -5,10 +5,11 @@ const mongoose = require('mongoose');
 const PORT = process.env.PORT || 5000;
 dotenv.config();
 app.use(express.json());
+const authRoute = require('./routes/auth');
 
 mongoose.connect(process.env.MONGO_URL).then(console.log('Connected to MongoDB')).catch(err => console.log(err));
 
-app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/auth', authRoute);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
