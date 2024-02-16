55130917297f33bfdd1f26af7696da2ac0babc21
ellie-ctrl35 <146957037+ellie-ctrl35@users.noreply.github.com>
2024-02-16 02:49:47 +0000

    Update response message in handleNewUser function

--- controllers/authcontroller.js
+++ controllers/authcontroller.js
@@ -14,7 +14,7 @@ const handleNewUser = async (req, res) => {
         });
 
         const user = await newUser.save();
-        res.status(200).json(user);
+        res.status(200).json("success");
     } catch (err) {
         res.status(500).json(err);
     }
