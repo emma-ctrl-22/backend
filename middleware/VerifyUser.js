74c92407e2d322f2deedaa4876352bfa0a2e6edb
ellie-ctrl35 <146957037+ellie-ctrl35@users.noreply.github.com>
2024-02-17 01:14:19 +0000

    Add user verification middleware

--- middleware/VerifyUser.js
+++ middleware/VerifyUser.js
@@ -1,5 +1,22 @@
-
+const dotenv = require('dotenv');
+dotenv.config();
+const jwt = require('jsonwebtoken');
 
 const verifyUser = (req, res, next) => {
-  
+  const token = req.cookies.accessToken;
+  if (!token) {
+    return res.status(401).json("You need to Login");
+  }else{
+    jwt.verify(accessToken,process.env.JWT_SECRET,(err,decoded)=>{
+        if(err){
+            return res.json("token is wrong")
+        }else{
+            req.username = decoded.username;
+            req.phone = decoded.phone;
+            next()
+        }
+    })
+  }
 }
+
+module.exports={verifyUser};
